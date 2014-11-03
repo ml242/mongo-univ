d33cfc5e87d41252d6e3f496ce6c78f54aeebb4a
ml242 <mattlucas@gmail.com>
2014-11-03 17:14:15 -0500

    mongo callbacks are calling me crazy maybe

--- week_2/homework_2_2_manual_validation_version/hw2-2/hw2-2validate/app.js
+++ week_2/homework_2_2_manual_validation_version/hw2-2/hw2-2validate/app.js
@@ -3,25 +3,32 @@ var MongoClient = require('mongodb').MongoClient;
 MongoClient.connect('mongodb://localhost:27017/weather', function(err, db) {
     
 
+
+
+
+
     if(err){
      console.log("line 7" + err);   
     }
 
     var query = {};
-    var projection = { 'State' : 1, 'Temperature' : 1 };
-    var cursor = db.collection('data').find(query, projection);
-    console.log("cursor" + "\n" + cursor);
+    var projection = { 'State' : 1, 'Temperature' : -1 };
+    var sort = {"Temperature" : -1 };    
+    var cursor = db.collection('data').find(query, projection, sort);
+    console.log("cursor:" + cursor);
 
     var state = '';
     var operator = {'$set' : {'month_high' : true } };
 
     cursor.each(function(err, doc) {
-        if (err) throw err;
 
+        if (err) throw err;
+        console.log('Update callback complete') 
         if (doc == null) {
-            console.log("docs have value:" + doc);
+            console.log("closing database");
             db.close();
         } else if (doc.State !== state) {
+            console.log("doc:" + doc)
             // first record for each state is the high temp one
             state = doc.State;
 
